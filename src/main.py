1267a6320acc61a45bc940ff23600fc95d7f14c0
dcfabioneves-cmd <dc.fabioneves@gmail.com>
2025-12-08 10:18:44 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], # Em produção, restrinja isso
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
