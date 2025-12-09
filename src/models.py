1097367c4d69a0ebd57f2a5a2233822e10d2aff0
dcfabioneves-cmd <dc.fabioneves@gmail.com>
2025-12-09 19:09:52 -0300

    models.py

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+# src/models.py
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from src.database import Base
@@ -6,7 +7,7 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    full_name = Column(String)
-    hashed_password = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
