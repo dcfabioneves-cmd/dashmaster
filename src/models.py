1f2e1753a0f9e7a9c60851c57e227cfa4b331121
dcfabioneves-cmd <dc.fabioneves@gmail.com>
2025-12-10 13:22:21 -0300

    models.py

--- src/models.py
+++ src/models.py
@@ -11,3 +11,20 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+from sqlalchemy import ForeignKey, JSON
+from sqlalchemy.orm import relationship
+
+class Project(Base):
+    __tablename__ = "projects"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    spreadsheet_url = Column(String)
+    categories = Column(JSON) # Armazena lista de categorias
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+    owner = relationship("User", back_populates="projects")
+
+User.projects = relationship("Project", back_populates="owner")
