932bc0e0cf462b6287a21deb22c1f7f367ac47c4
dcfabioneves-cmd <dc.fabioneves@gmail.com>
2025-12-10 17:29:04 -0300

    auth_router.py

--- src/routers/auth_router.py
+++ src/routers/auth_router.py
@@ -8,6 +8,7 @@ from datetime import timedelta
 
 from src.database import get_db
 from src.models import User
+from src.auth import (
     verify_password, 
     get_password_hash, 
     create_access_token, 
